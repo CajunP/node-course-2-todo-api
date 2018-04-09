f5747d9635682a21b902735f3bedcbd145834556
Chulio <ivalasojarazo@gmail.com>
2018-04-09 14:09:18 -0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -25,7 +25,13 @@ app.post('/todos', (req, res) => {
   })
 });
 
-
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 
 app.listen(3000, () => {
@@ -33,7 +39,7 @@ app.listen(3000, () => {
 });
 
 
-
+module.exports = {app};
 
 
 
