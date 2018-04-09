f156bb3e51a275fbbe279e46e746c05c718a7bdf
Chulio <ivalasojarazo@gmail.com>
2018-04-09 17:15:24 -0300

    Setup 2

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
-
 var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
 
 var app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -23,7 +20,7 @@ app.post('/todos', (req, res) => {
     res.send(doc);
   }, (e) => {
     res.status(400).send(e);
-  })
+  });
 });
 
 app.get('/todos', (req, res) => {
@@ -34,53 +31,26 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
-app.get('/todos/:id', (req,res) => {
-  var id =req.params.id;
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-  Todo.findById(id).then((todo) => {
 
-    if(!todo){
-      res.status(404).send();
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
     res.send({todo});
-  }, (e) => {
+  }).catch((e) => {
     res.status(400).send();
   });
-
 });
 
-
-app.listen(3000, () => {
-  console.log(`Started on port ${port}`);
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
 });
-
 
 module.exports = {app};
-
-
-
-
-
-
-// var otherTodo = new Todo({text: "Practicar kanji"});
-//
-// otherTodo.save().then((doc) => {
-//   console.log('Saved todo',doc);
-// }, (e) => {
-//   console.log('Unable to save todo',e);
-// });
-
-// User
-
-
-// var newUser = new User({email:"ivalasojarazo@gmail.com"});
-//
-// newUser.save().then((doc) => {
-//   console.log("Saved user", doc);
-// }, (e) => {
-//   console.log("Unable to save user", e);
-// });
